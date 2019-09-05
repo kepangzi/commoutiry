3b16f1dc1ddaef1c13ffc3b0526071ea120b9dab
沈潮 <992532450@qq.com>
2019-09-05 16:48:24 +0800

    first commit

---
+++ src/main/java/file/majiang/commounity/CommounityApplication.java
@@ -0,0 +1,13 @@
+package file.majiang.commounity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommounityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommounityApplication.class, args);
+    }
+
+}
---
+++ src/main/java/file/majiang/commounity/controller/IndexController.java
@@ -0,0 +1,15 @@
+package file.majiang.commounity.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value = "/")
+    public String index(){
+        return "index";
+    }
+}
