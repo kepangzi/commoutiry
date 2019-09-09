0eeaffaab4fb926f66400fad43fbd1d0c05f0ab9
kepangzi <992532450@qq.com>
2019-09-09 16:06:52 +0800

    add check cookies

--- src/main/java/file/majiang/commounity/controller/IndexController.java
+++ src/main/java/file/majiang/commounity/controller/IndexController.java
@@ -1,15 +1,38 @@
 package file.majiang.commounity.controller;
 
+import file.majiang.commounity.mapper.UserMapper;
+import file.majiang.commounity.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 @Controller
 public class IndexController {
+    @Autowired
+    UserMapper userMapper;
 
     @GetMapping(value = "/")
-    public String index(){
+    public String index(HttpServletRequest request){
+        Cookie[] cookies = request.getCookies();
+
+        for (Cookie cookie : cookies) {
+            if(StringUtils.pathEquals(cookie.getName(),"token")){
+                //获取跳转带过来的token
+                String token = cookie.getValue();
+                //获取数据库查询出来的token
+                User user = userMapper.findByToken(token);
+                if (user!=null){
+                    request.getSession().setAttribute("user",user);
+                    break;
+                }
+            }
+        }
         return "index";
     }
 }
--- src/main/java/file/majiang/commounity/mapper/UserMapper.java
+++ src/main/java/file/majiang/commounity/mapper/UserMapper.java
@@ -3,10 +3,14 @@ package file.majiang.commounity.mapper;
 import file.majiang.commounity.model.User;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface UserMapper {
 
     @Insert("insert into user(name,account_id,token,create_time,gmt_modified) values(#{name},#{accountId},#{token},#{createTime},#{gmtModified})")
     void insert(User user);
+    @Select("select * from user where token =#{token}")
+    User findByToken(@Param("token") String token);
 }
